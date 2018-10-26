e981e27fe3f173ca6b79c4d161d5198e959c4ce0
Cristian <cfernandez@artool.cl>
2018-10-26 16:11:30 -0300

    InitialLoginProject

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_135229) do
+ActiveRecord::Schema.define(version: 2018_10_26_190818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,24 @@ ActiveRecord::Schema.define(version: 2018_10_26_135229) do
     t.datetime "updated_at", null: false
     t.bigint "ticket_id"
     t.index ["ticket_id"], name: "index_answers_on_ticket_id"
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.bigint "fb_ad_account_id"
+    t.string "ga_ad_account_id"
+    t.integer "fb_dollar_conv", default: 1, null: false
+    t.integer "ga_dollar_conv", default: 1, null: false
+    t.string "main_color"
+    t.boolean "is_enabled", default: true
+    t.datetime "last_update"
+  end
+
+  create_table "clients_users", id: false, force: :cascade do |t|
+    t.bigint "client_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["client_id", "user_id"], name: "index_clients_users_on_client_id_and_user_id"
+    t.index ["user_id", "client_id"], name: "index_clients_users_on_user_id_and_client_id"
   end
 
   create_table "mailers", force: :cascade do |t|
@@ -58,6 +76,16 @@ ActiveRecord::Schema.define(version: 2018_10_26_135229) do
     t.text "campus"
     t.text "case_type"
     t.index ["person_id"], name: "index_tickets_on_person_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
