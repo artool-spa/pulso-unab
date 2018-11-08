d503ce281426955f3c88295f78f128be818c0d05
Cristian <cfernandez@artool.cl>
2018-11-08 14:34:30 -0300

    Fix badly written comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,9 @@ require 'factory_bot'
 
 RSpec.describe User, type: :model do
     subject { create :user }
-  
+    
     it 'sends an email' do
-
       expect { subject.send_instructions }
-      #Verify that the email counts in the delivery array, increased by one
         .to change { ActionMailer::Base.deliveries.count }.by(1)
         byebug
     end
