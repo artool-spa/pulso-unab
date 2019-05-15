4adadcfbdf3f7c5184807b9851fbe708953a45f0
Cristian <cfernandez@artool.cl>
2019-05-15 18:27:16 -0400

    add mailer design footer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'f691a35a.ngrok.io'}
-  config.action_mailer.asset_host = 'https://f691a35a.ngrok.io'
+  config.action_mailer.default_url_options = { host: '6633934f.ngrok.io'}
+  config.action_mailer.asset_host = 'https://6633934f.ngrok.io'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = Rails.application.credentials.artool_smtp
 end
