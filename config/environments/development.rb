38d26668959044bc3f7fa179b4eec52fb7fcfb56
Cristian <cfernandez@artool.cl>
2019-05-15 10:30:37 -0400

    add mailer design

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: '8a6d3c96.ngrok.io'}
-  config.action_mailer.asset_host = 'https://8a6d3c96.ngrok.io'
+  config.action_mailer.default_url_options = { host: 'f691a35a.ngrok.io'}
+  config.action_mailer.asset_host = 'https://f691a35a.ngrok.io'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = Rails.application.credentials.artool_smtp
 end
