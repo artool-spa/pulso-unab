2bea4c781fe69ae17f0701f0b726f41a06934119
Cristian <cfernandez@artool.cl>
2019-05-22 16:02:00 -0400

    production config

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -11,11 +11,11 @@ role :db,  %w{ubuntu@52.37.95.9}
 load File.expand_path('../../secrets_deploy.rb', __FILE__)
 
 set :default_env, {
-  database_url:  "postgresql://#{fetch(:dbuser)}:#{fetch(:dbpassword)}@localhost/#{fetch(:dbname)}"
+  database_url:  "postgresql://#{fetch(:dbuser)}:#{fetch(:dbpassword)}@#{fetch(:dbhost)}/#{fetch(:dbname)}"
 }
 
 set :ssh_options, {
-  keys: %w(~/.ssh/qp.pem),
+  keys: %w(~/.ssh/funnel_key.pem),
   forward_agent: true,
 #  auth_methods: %w(password)
 }
