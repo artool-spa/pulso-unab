fbfb8987763e74ac064e3f2e0de0e81e41a49b48
Cristian <cfernandez@artool.cl>
2019-07-02 17:27:01 -0400

    Stop mails send

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,10 @@
 class Person < ApplicationRecord
   has_many :tickets
   has_many :log_mailer_sends
+
+  def self.person_tickets_count
+    Person.all.each do |person|
+      puts "person_name: #{person.full_name} #{person.tickets.count}" if person.tickets.count > 10
+    end
+  end
 end
