4fd6fca983cf802b4aa8dae2323941a975ce9336
Cristian <cfernandez@artool.cl>
2019-07-04 11:30:02 -0400

    WIP

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,11 @@
 class Person < ApplicationRecord
   has_many :tickets, dependent: :destroy
-  has_many :log_mailer_sends
+  has_many :log_mailer_sends, dependent: :destroy
 
   def self.person_tickets_count
     Person.all.each do |person|
       puts "person_name: #{person.full_name} #{person.tickets.count}" if person.tickets.count > 10
     end
   end
+  
 end
