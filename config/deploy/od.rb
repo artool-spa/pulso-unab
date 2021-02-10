00e83aa656cb120ea55e9eec5477c814ab471356
Jean-Pierre M <hiperjp@gmail.com>
2021-02-10 14:42:05 -0300

    Agregando deploy para rama on-demand

---
+++ config/deploy/od.rb
@@ -0,0 +1,21 @@
+set :stage, :production
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, "on-demand"
+
+role :app, %w{ubuntu@34.214.35.122}
+role :web, %w{ubuntu@34.214.35.122}
+role :db,  %w{ubuntu@34.214.35.122}
+
+load File.expand_path('../../secrets_deploy.rb', __FILE__)
+
+set :default_env, {
+  database_url:  "postgresql://#{fetch(:dbuser)}:#{fetch(:dbpassword)}@#{fetch(:dbhost)}/#{fetch(:dbname)}"
+}
+
+set :ssh_options, {
+  keys: %w(~/.ssh/funnel_key.pem),
+  forward_agent: true,
+#  auth_methods: %w(password)
+}
