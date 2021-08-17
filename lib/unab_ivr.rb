1b9fe587438e5732a3db10c34b3fe24d294da28b
Sebastian Campos <sebastian94.scr@gmail.com>
2021-08-17 13:00:09 -0400

    test method servicio unab

--- lib/unab_ivr.rb
+++ lib/unab_ivr.rb
@@ -3,6 +3,17 @@ require 'date'
 require 'net/http'
 
 class UnabIvr
+  def initialize
+    @client = Savon.client(wsdl: 'https://neotel.unab.cl/neoapi/webservice.asmx?WSDL')
+    #@contact = Savon.client(wsdl: 'http://200.29.164.74:8059/WS_CRMContactosCasos.asmx?WSDL')
+  end
+
+  def get_ExecuteTask02(idTask, param1, param2)
+    response = @client.call(:ExecuteTask02, message: { idTask: idTask, param1: param1, param2: param2 })
+    p response
+    #response.body[:wm_find_by_rut_response][:wm_find_by_rut_result][:output]
+  end
+
   def get_ivr_data(date)
     #server_offset = 0
     #time = (Time.now - server_offset.hours).strftime("%d%m%Y%H")
