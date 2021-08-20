5608885953f174a057f9e173451e9a6c8ffa7f72
Bastián Herdocio <bherdocio@artool.cl>
2021-08-20 17:20:34 -0400

    IVR instance ready

--- lib/unab_ivr.rb
+++ lib/unab_ivr.rb
@@ -8,24 +8,23 @@ class UnabIvr
     #@contact = Savon.client(wsdl: 'http://200.29.164.74:8059/WS_CRMContactosCasos.asmx?WSDL')
   end
 
-  def get_ExecuteTask02(idTask, param1, param2)
-    response = @client.call(:ExecuteTask02, message: { idTask: idTask, param1: param1, param2: param2 })
-    p response
-    #response.body[:wm_find_by_rut_response][:wm_find_by_rut_result][:output]
+  def get_ivr_data(idTask, param1, param2)
+    response = @client.call(:execute_task02, message: { idTask: idTask, param1: param1, param2: param2 })
+    response.body[:execute_task02_response][:execute_task02_result]
   end
 
-  def get_ivr_data(date)
-    #server_offset = 0
-    #time = (Time.now - server_offset.hours).strftime("%d%m%Y%H")
-    time = Time.now.utc.strftime("%d%m%Y%H")
-    hash = Digest::SHA256.hexdigest(time+"unab.2018")
-    #2602201914
-    #timeParam = "28022019"
-    timeParam = date.to_date.strftime("%d%m%Y")
-    #puts("> "+time)
+  # def get_ivr_data(date)
+  #   #server_offset = 0
+  #   #time = (Time.now - server_offset.hours).strftime("%d%m%Y%H")
+  #   time = Time.now.utc.strftime("%d%m%Y%H")
+  #   hash = Digest::SHA256.hexdigest(time+"unab.2018")
+  #   #2602201914
+  #   #timeParam = "28022019"
+  #   timeParam = date.to_date.strftime("%d%m%Y")
+  #   #puts("> "+time)
 
-    url = URI.parse("http://epa.mets.cl/unab/index.php?t=" + timeParam + "&h=" + hash)
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
-  end
+  #   url = URI.parse("http://epa.mets.cl/unab/index.php?t=" + timeParam + "&h=" + hash)
+  #   req = Net::HTTP::Get.new(url.to_s)
+  #   res = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+  # end
 end
